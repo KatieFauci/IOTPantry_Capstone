76609f633795eb94e0d0fa6111d1347549be323e
kessmith <skeshawn86@gmail.com>
2020-11-03 22:55:40 -0500

    Updated Server.py file and added Home.html file

--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask import render_template
+from frlask_mysqld import MySQL
+
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'Inventory_Database'
+
+mysql = MySQL(app)
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to our IOT Pantry System'
-
-
+def Home():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM food_type")
+    fetchdata = cur.fetchall()
+    cur.close()
+    return redner_template('home.html', data = fetchdata)
