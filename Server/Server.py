071a8ead49ede79c5fba8620a5bbe27cfb60ba3f
kessmith <skeshawn86@gmail.com>
2020-11-24 01:17:26 -0500

    Updated Server.py (needs to be improved)

--- Server/Server.py
+++ Server/Server.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask import render_template
-import Main_Application.IOTPantry
+import os
 # import Main_Application --> Will be importing the main application file that will be going here
 # Main app needs to run in the background
 
@@ -8,8 +7,8 @@ import Main_Application.IOTPantry
 app = Flask(__name__)
 @app.route('/')
 def run_script():
-    file = open(r'/home/ubuntu/Flask-project/IOTPantry_Capstone/Main_Application/Main_Application.py', 'r').read()
-    return exec(file)
+    file = '/home/ubuntu/Capstone/IOTPantry_Capstone/Main_Application/IOTPantry/IOTPantry.py'
+    os.startfile(file)
 
 if __name__ == '__main__':
     app.run(debug=True)
