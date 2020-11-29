5256fc0f34806cf881e8220ff8f924bfb04b30a2
kessmith <skeshawn86@gmail.com>
2020-11-28 19:51:20 -0500

    Updated flask_server.,py

--- Main_Application/IOTPantry/flask_server.py
+++ Main_Application/IOTPantry/flask_server.py
@@ -5,13 +5,13 @@ DATABASE = 'inventory.db'
 app = Flask(__name__)
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(Flask, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = Flask._database = sqlite3.connect(DATABASE)
     return db
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(Flask, '_database', None)
     if db is not None:
         db.close()
