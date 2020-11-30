c300c1389d5069cf2f757237466d804307cc5f5f
KatieF501 <fauci@hartford.edu>
2020-11-30 13:46:08 -0500

    flask testing

--- flask_server/flask_test.py
+++ flask_server/flask_test.py
@@ -1,7 +1,8 @@
 import sqlite3
 from flask import g
 
-DATABASE = 'inventory.db'
+app = Flask(__name__)
+DATABASE = '/path/to/database.db'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -14,3 +15,6 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+        
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
