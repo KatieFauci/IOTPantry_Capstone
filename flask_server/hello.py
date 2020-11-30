a5e865a68ac45531ab03017383e377a1cfe3123c
KatieF501 <fauci@hartford.edu>
2020-11-30 13:38:52 -0500

    flask testing

--- flask_server/hello.py
+++ flask_server/hello.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
